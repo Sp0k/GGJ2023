4ca20bbf75d1872966897e89575852d190b36457
lucy222w <lc549851@dal.ca>
2023-02-04 16:08:44 -0400

    start screen

---
+++ Assets/Scripts/startGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
