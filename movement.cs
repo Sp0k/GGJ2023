9f1454354c2990d21e6bc214668c65f889c6d0ae
steve040415 <124400259+steve040415@users.noreply.github.com>
2023-02-04 10:07:04 -0330

    limited movement

---
+++ movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float moveSpeed;
+    public float maxHeight;
+    public float minHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if ((Input.GetKey(KeyCode.UpArrow) == true && transform.position.y<maxHeight))
+        {
+            myRigidbody.transform.position = new Vector3(myRigidbody.transform.position.x, myRigidbody.transform.position.y + moveSpeed * Time.deltaTime, myRigidbody.transform.position.z);
+            myRigidbody.transform.Translate(Vector3.forward * Time.deltaTime);
+
+        }
+        if((Input.GetKey(KeyCode.DownArrow) == true && transform.position.y>minHeight))
+        {
+            //down
+            myRigidbody.transform.position = new Vector3(myRigidbody.transform.position.x, myRigidbody.transform.position.y - moveSpeed * Time.deltaTime, myRigidbody.transform.position.z);
+            myRigidbody.transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //left
+            myRigidbody.transform.position = new Vector3(myRigidbody.transform.position.x - moveSpeed * Time.deltaTime, myRigidbody.transform.position.y, myRigidbody.transform.position.z);
+            myRigidbody.transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //left
+            myRigidbody.transform.position = new Vector3(myRigidbody.transform.position.x + moveSpeed * Time.deltaTime, myRigidbody.transform.position.y, myRigidbody.transform.position.z);
+            myRigidbody.transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+
+    }
+}
