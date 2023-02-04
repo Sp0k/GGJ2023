afe498270060cbb500a205f3c1ebc23c6e6da262
Anthony <anthonydaniel123@live.com>
2023-02-03 20:36:13 -0400

    time loop

--- Assets/WorldGeneration.cs
+++ Assets/WorldGeneration.cs
@@ -1,32 +1,46 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldGeneration : MonoBehaviour
 {
     public GameObject[] rooms;
     public Transform player;
+    public List<GameObject> roomsList;
 
-    public int levelCount = 0;
+    private int roomCount = 0;
 
     private System.Random random;
 
     void Start()
     {
+        random = new System.Random();
+        roomsList = new List<GameObject>();
+
         GameObject initialRoom = Instantiate(rooms[0]);
         initialRoom.transform.position = Vector3.zero;
+        roomsList.Add(initialRoom);
 
-        GameObject nextRoom = Instantiate(rooms[random.Next(1, rooms.Length)]);
-        int x = 0; // levelLength / 2
+        GameObject nextRoom = Instantiate(rooms[RandomIndex()]);
+        int x = 20; 
         nextRoom.transform.position = new Vector3(x, 0);
+        roomsList.Add(nextRoom);
 
-        levelCount += 2;
+        roomCount += 2;
     }
 
     void Update()
     {
-        // Check if player entered next level:
-        //     block previous level
-        //     spawn random new level
-        //     change position using levelCount
-        //     add level to level count
+        if (player.position.x > 10 + ((roomCount - 1) * 20) -1)
+        //if (player.position.x > )
+        {
+            // To do: block player from going back
+            Destroy(roomsList[0]);
+            roomsList.RemoveAt(0);
+            roomsList.Add(Instantiate(rooms[RandomIndex()]));
+            roomsList[1].transform.position = new Vector3(20 + ((roomCount - 2) * 40), 0);
+            roomCount++;
+        }
     }
+
+    int RandomIndex() => random.Next(1, rooms.Length - 1);
 }
