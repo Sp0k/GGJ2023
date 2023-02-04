3e8a92fbce6d3f03dd1dc942351140ce4ecde4ce
Gabriel Savard <123707889+Gabriel-Savard@users.noreply.github.com>
2023-02-04 12:18:32 -0400

    Added a dash button

---
+++ Movement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public Animator animator;
+    public float moveSpeed;
+
+    private Vector2 movement;
+
+    // Dash variables
+    private float activeMoveSpeed;
+    public float dashSpeed;
+
+    public float dashLength = .5f, dashCooldown = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+
+    void Start()
+    {
+        activeMoveSpeed = moveSpeed;
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Move", movement.x);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + movement * activeMoveSpeed * Time.fixedDeltaTime);
+    }
+}
