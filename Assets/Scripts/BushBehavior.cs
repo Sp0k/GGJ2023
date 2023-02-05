06a05306d3c7b31a66b2f4e27bcff982f1e34b62
Gabriel-Savard <gabrielsavard.prog@gmail.com>
2023-02-05 15:19:46 -0400

    George's behavior

---
+++ Assets/Scripts/BushBehavior.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushBehavior : MonoBehaviour
+{
+public GameObject ghost;
+
+    public float range;
+    public float speed;
+    public int health;
+
+    public void Update()
+    {
+        float dist = Vector3.Distance(EnemySpawner.player.position, transform.position);
+
+        if (dist <= range)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, EnemySpawner.player.position, Time.deltaTime);
+
+        }
+    }
+
+    public void Hit()
+    {
+        health--;
+
+        GameManager.IncrementScore();
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Instantiate(ghost, transform.position, Quaternion.identity);
+        Destory(GameObject);
+    }
+}
