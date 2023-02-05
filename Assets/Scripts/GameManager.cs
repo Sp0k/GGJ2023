2f964495915255c0d525597c748aecda2d883f12
Anthony <anthonydaniel123@live.com>
2023-02-05 10:14:32 -0400

    Edited gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI highScoreText;
     public TextMeshProUGUI highestLevelText;
+    public TextMeshProUGUI scoreText;
 
     public static int score { get; private set; }
     public static int highScore { get; private set; }
@@ -21,18 +22,22 @@ public class GameManager : MonoBehaviour
         highScore = newHighScore;
 
         GameManager gameManager = FindObjectOfType<GameManager>();
-        gameManager.highScoreText.text = highScore.ToString();
+        gameManager.highScoreText.text = "HighScore: " + highScore.ToString();
     }
 
     public static void UpdateHighestLevel(int newLevel)
     {
         highestLevel = newLevel;
 
-
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        gameManager.highestLevelText.text = "Highest Level: " + highestLevel.ToString();
     }
 
-    public static void IncrementScore()
-    {
+    public static void IncrementScore() 
+    { 
+        score++;
 
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        gameManager.scoreText.text = "Score: " + score.ToString();
     }
 }
