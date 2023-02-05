02189b9d706061b93e28c997f8ed301c582b90e4
lucy222w <lc549851@dal.ca>
2023-02-05 09:41:05 -0400

    game over screen no code

--- Assets/Scripts/playAgain.cs
+++ Assets/Scripts/playAgain.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class startGame : MonoBehaviour
+public class playAgain : MonoBehaviour
 {
     public void LoadGame()
     {
         SceneManager.LoadScene(1);
-      //  Application.LoadLevel(1);
+        //  Application.LoadLevel(1);
     }
     // Start is called before the first frame update
     void Start()
--- Assets/score.cs
+++ Assets/score.cs
@@ -1,15 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class startGame : MonoBehaviour
+public class score : MonoBehaviour
 {
-    public void LoadGame()
-    {
-        SceneManager.LoadScene(1);
-      //  Application.LoadLevel(1);
-    }
     // Start is called before the first frame update
     void Start()
     {
