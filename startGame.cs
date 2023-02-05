35a8d5ebf53294a94794c9f5234683743921aca5
steve040415 <124400259+steve040415@users.noreply.github.com>
2023-02-05 10:25:47 -0330

    Add files via upload

---
+++ startGame.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public bool isStart;
+public bool isCredits;
+
+public class startGame : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    void OnMouseUp() {
+        if(isStart) {
+            SceneManager.LoadScene(1);
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
