87098cb49ba2dcde843843c4fde8f43ba194216d
steve040415 <124400259+steve040415@users.noreply.github.com>
2023-02-05 12:33:40 -0330

    Add files via upload

---
+++ Assets/Scripts/startGame.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    public bool isStart;
+    public bool isCredits;
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+      
+    }
+    public void  OnClick() {
+        if(isStart) {
+            SceneManager.LoadScene(1);
+           
+        }
+         Debug.Log("ur mom");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
