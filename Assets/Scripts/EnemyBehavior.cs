79c92e2431a6df3c9742b21577d3da57372ced2e
Gabriel-Savard <gabrielsavard.prog@gmail.com>
2023-02-05 12:07:53 -0400

    minor update on enemy behavior

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -6,9 +6,23 @@ public class EnemyBehavior : MonoBehaviour
     public float speed;
     public int health;
 
+    public SpriteRenderer renderer;
+
     void Update()
     {
+        float oldPos = transform.position.x;
         float dist = Vector3.Distance(EnemySpawner.player.position, transform.position);
+
+        float direction = oldPos - transform.position.x;
+
+        if (direction < 0)
+        {
+            renderer.flipX = true;;
+        }
+        else if (direction > 0)
+        {
+            renderer.flipX = false;
+        }
 
         if (dist <= range)
         {
